d04bc153289e800a324c620fd540eb66e91ce6d1
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-09-27 23:46:52 +0600

    triple or double (equal sign) practice done.

---
+++ triple.js
@@ -0,0 +1,13 @@
+// always use triple
+const a = 2;
+const b = "2";
+
+// triple equal check korar khetre valuer type tao check kore
+if (a === b) {
+  console.log("value are same");
+} else {
+  console.log("value are not  same");
+}
+
+// type coercion
+// type conversion

91950c9402ee366537d9ed01f2dfe75c69b9597b
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-09-28 00:22:49 +0600

    closure in javascript practice done.

---
+++ closure.js
@@ -0,0 +1,13 @@
+function kitchen() {
+  let roast = 0;
+  return function () {
+    roast++;
+    return roast;
+  };
+}
+
+const firstServer = kitchen();
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());
+console.log(firstServer());

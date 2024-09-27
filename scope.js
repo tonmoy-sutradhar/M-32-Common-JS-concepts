df47e46a7a42cdb83468477267954a8c5dab5c7d
Tonmoy <tonmoysutradhar2002@gmail.com>
2024-09-28 00:05:33 +0600

    scope concept practice doen.

---
+++ scope.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+  const total = a + b;
+  if (b < 5) {
+    const sum = 4 + a + b; //sum ta k if block er bahire pao jabe na
+  }
+  console.log(a, b);
+}
+console.log(a, b); //output: a is not defined
+console.log(total); //output: total is not defined
+add(4, 6);
